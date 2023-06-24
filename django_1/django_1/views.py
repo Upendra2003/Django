fc5f98fedcb470c5522fdfd994220b762640ad83
Upendra2003 <upendrakarimi2003@gmail.com>
2023-06-24 17:52:05 +0530

    django 1

--- django_1/django_1/views.py
+++ django_1/django_1/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(Response):
-    return render(Response,'index.html')
+def index(request):
+    return render(request,'index.html')
+
+def analyze(request):
+    text=request.GET.get('text','default')
+    remove_punctuation=request.GET.get('remove_punctuation','off')
+    capitalize_text=request.GET.get('capitalize_text','off')
+    print(remove_punctuation)
+    if remove_punctuation=='on':
+        punctuations='''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
+        analyzed_text=''
+        for char in text:
+            if char not in punctuations:
+                analyzed_text+=char
+        result={'analyzed_text':analyzed_text,'operation':'Removed Punctuation'}
+        return render(request,'analyze.html',result)
+    elif capitalize_text=='on':
+        analyzed_text=text.upper()
+        result={'analyzed_text':analyzed_text,'operation':'Upper case'}
+        return render(request,'analyze.html',result)
+    return '404'
