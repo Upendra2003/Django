1fa02f3fb38e5bc1f38a29d6d7101025c77143e1
Upendra2003 <upendrakarimi2003@gmail.com>
2023-06-28 16:06:22 +0530

    django 2

--- django_2/shop/models.py
+++ django_2/shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_id=models.AutoField(primary_key=True)
+    product_name=models.CharField(max_length=50,default='')
+    product_description=models.CharField(max_length=100,default='default')
+    pub_date=models.DateField()
