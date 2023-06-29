f04eb7bc6fc2cddb64bbc6ff23287b15ad451e3d
Upendra2003 <upendrakarimi2003@gmail.com>
2023-06-29 23:54:31 +0530

    django 2

--- django_2/shop/views.py
+++ django_2/shop/views.py
@@ -3,12 +3,15 @@ from .models import Product
 
 # Create your views here.
 def index(req):
-    get_data=Product.objects.all()
-    get_category=Product.objects.values_list('product_category')
-    get_len=len(get_data)
+    get_category=Product.objects.values('product_category','product_id')
+    all_cats={item['product_category'] for item in get_category}
+    for category in all_cats:
+        all_products=Product.objects.filter(product_category=category).values()
+    list(all_products)
+    # for i,j in all_products:
+    #     print(i,j)
+    print(all_products)
     context={
-        # "category":get_category,
-        "data":get_data,
-        # "length":get_len
+        "data":all_products,
     }
     return render(req,'shop/index.html',context)
