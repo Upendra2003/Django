261a985ae524ffb9c13b3e22ab867f77da46a223
Upendra2003 <upendrakarimi2003@gmail.com>
2023-07-13 00:03:03 +0530

    django_6

--- django_6/result/admin.py
+++ django_6/result/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Branch,Student,Subjects
 
 # Register your models here.
+admin.site.register(Branch)
+admin.site.register(Student)
+admin.site.register(Subjects)
--- django_6/result/models.py
+++ django_6/result/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Branch(models.Model):
+    branch=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.branch
+
+class Subjects(models.Model):
+    subject_name=models.CharField(max_length=100,null=True,blank=True)
+    marks_obtained=models.IntegerField(null=True,blank=True)
+
+    def __str__(self):
+        return self.subject_name
+
+class Student(models.Model):
+    student_name=models.CharField(max_length=100,null=True,blank=True)
+    student_id=models.CharField(max_length=10,null=True,blank=True)
+    student_branch=models.OneToOneField(Branch,on_delete=models.CASCADE,blank=True,null=True)
+    student_subjects=models.ForeignKey(Subjects,on_delete=models.CASCADE,null=True,blank=True)
+
+    def __str__(self):
+        return self.student_name
+    
